eb6f5417589a3ae2017ecc25128aeb0efcefd6d6
rogerio-pio <rogeriomachadopio@gmail.com>
2025-03-19 13:07:10 -0300

    Header

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,15 +1,17 @@
 import { Text, View } from "react-native";
+import { Header } from "../components/header";
+import  Constants from 'expo-constants'
+
+const statusBarHeight = Constants.statusBarHeight;
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>rogerio.</Text>
+    <View style={{flex: 1}} className="bg-slate-50">
+
+      <View className="w-full px-4 bg-white h-16 wstyle={{ paddingTop: statusBarHeight }}" >
+        <Header/>
+      </View>
+
     </View>
   );
 }
---
+++ src/components/header/index.tsx
@@ -0,0 +1,28 @@
+import { View, Pressable, Image } from 'react-native';
+import {Ionicons, FontAwesome } from '@expo/vector-icons';
+
+export function Header() {
+ return (
+   <View className="flex-row items-center justify-between w-full h-full px-4">
+        <Pressable>
+            <Ionicons name='menu' size={24} color={"#0047BB"} />
+        </Pressable>
+
+        {/* Logo */}
+        <View className="absolute left-1/2 -translate-x-1/2">
+            <Image 
+            source={require('../../../assets/images/Logo.png')} // Verifique o caminho exato da sua imagem
+            style={{ width: 100, height: 40 }} // Ajuste conforme necessário
+            resizeMode="contain" 
+            />
+      </View>
+
+        {/* Ícone do WhatsApp */}
+        <Pressable>
+            <FontAwesome name="whatsapp" size={24} color={"#0047BB"} /> 
+        </Pressable>
+
+   </View>
+
+  );
+}
